0b4394c7d69ba6f06dcc81860bfe3ed078d82616
gabrielrleal <gabrielxleal@gmail.com>
2021-05-14 12:03:49 -0300

    lab2 refazendo

---
+++ src/main/java/br/ufpb/dsc/DSCLAB2/DscLab2Application.java
@@ -0,0 +1,13 @@
+package br.ufpb.dsc.DSCLAB2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscLab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpb/dsc/DSCLAB2/DscLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dsc.DSCLAB2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscLab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
